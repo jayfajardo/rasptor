3486034eefd49e6569aaa0018d4433c9627542b4
Jay Fajardo <jay@proudcloud.net>
2015-06-20 20:50:27 +0800

    drop the prompt switch

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@
 /bin/echo "This script will auto-setup a Tor proxy for you. It is recommend that you
 run this script on a fresh installation of Raspbian."
 
-read -p "Press [Enter] key to begin.."
+read "Press [Enter] key to begin.."
 
 # Set up default variables
 IP_ADDRESS="192.168.42.1"
@@ -28,7 +28,7 @@ CHANNEL="6"
 
 # read -p "Enter the IP Address you wish to assign to your RaspTor <${IP_ADDRESS}> :" IP_ADDRESS 
 
-read -p "Enter your desired WLAN SSID <${SSID}> :" ssid 
+read -p "Enter your desired WLAN SSID <${SSID}> :" SSID 
 
 read -p "Enter your desired WPA2 key <${WPA2}> :}" WPA2
  
